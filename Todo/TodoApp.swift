b0dfe263a1b1c5dca521ef591bfec713908decf4
devraysingh <raysahni98@gmail.com>
2023-07-26 19:48:35 -0400

    Initial Commit

---
+++ Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by ray on 7/26/23.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
